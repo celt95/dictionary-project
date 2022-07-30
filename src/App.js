66bfaa8d59bdbbe0b02bc393a81563a8cc95056c
celt95 <100221513+celt95@users.noreply.github.com>
2022-07-30 14:23:34 +0300

    initial commit for project setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">Hello from Header</header>
+      <button className="btn btn-primary">Hello</button>
+    </div>
+  );
+}
+
+export default App;
